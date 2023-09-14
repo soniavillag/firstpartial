19fd57af4d7269da39aad380d96c3de29115298d
soniavillag <145053431+soniavillag@users.noreply.github.com>
2023-09-14 12:49:04 -0600

    Create codingrush.py

---
+++ codingrush.py
@@ -0,0 +1,9 @@
+#inversion word for desserts 
+def dessert():
+  print("give me a word")
+  word = input()
+  print("give me a second word")
+  inverted_word = word[::-1]
+  print(inverted_word)
+  print ("Bye! See you next time! ;)")
+dessert()
